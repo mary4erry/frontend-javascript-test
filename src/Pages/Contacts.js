7480b9209b77015cd96e3e1921af53ddf2754bbe
Maria Vatolina <mary4erry@yandex.ru>
2021-02-02 23:34:08 +0300

    add pagination

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { Grid, Paper, 
-         Container } from '@material-ui/core'
+         Container, TablePagination } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import { useContacts } from './useContacts'
 import { ContactsTable } from '../Components/Table/ContactsTable'
 import { ContactInfo } from '../Components/ContactInfo/ContactInfo'
-import Switch from '../Components/Switch/Switch';
+import Switch from '../Components/Switch/Switch'
 
 const useStyles = makeStyles((theme) => ({
    root: {
@@ -17,11 +17,22 @@ export const Contacts = () => {
    const classes = useStyles()
    const [ isDataSelected, setIsDataSelected ] = useState(false)
    const [ isRowSelected, setIsRowSelected ] = useState(false)
-   const [sortDirection, setSortDirection] = useState(true)
-   const [fieldData, setfieldData] = useState('')
-   const [contactInfo, setContactInfo] = useState('')
-   const [url, setUrl] = useState('')
+   const [ sortDirection, setSortDirection ] = useState(true)
+   const [ fieldData, setfieldData ] = useState('')
+   const [ contactInfo, setContactInfo ] = useState('')
+   const [ url, setUrl ] = useState('')
    const contacts = useContacts( url )
+   const [page, setPage] = useState(0)
+   const [rowsPerPage] = useState(50)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  }
+
+   const totalUsersCount = contacts.data.length
+   const indexOfFirsRow = page * rowsPerPage 
+   const indexOfLasRow = (page + 1) *  rowsPerPage  
+   const currentUsers = contacts.data.slice(indexOfFirsRow, indexOfLasRow)
 
    const onSort = (field) => {
       const copyData = contacts.data.concat()
@@ -45,7 +56,7 @@ export const Contacts = () => {
       setUrl(url)
       setIsDataSelected(true)
    }
-
+ 
    return ( 
       <Container className={classes.root}>
          <Grid container spacing={3} style={{display: "flex", justifyContent: "center"}}>
@@ -58,6 +69,16 @@ export const Contacts = () => {
                      <Paper> Search </Paper>
                   </Grid>
                   <Grid item xs={12}>
+                     <TablePagination 
+                        component="div"
+                        rowsPerPageOptions={[50]}
+                        count={totalUsersCount}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onChangePage={handleChangePage}
+                        />
+                  </Grid>
+                  <Grid item xs={12}>
                      {(() => {
                         if (contacts.isLoading) {
                            return <div> ...loading </div>
@@ -66,7 +87,7 @@ export const Contacts = () => {
                            return <div> error! </div>
                         }
                         return <ContactsTable 
-                           data = {contacts.data}
+                           data = {currentUsers}
                            onSort = {onSort}
                            sortDirection = {sortDirection}
                            showInfo = {showInfo}
@@ -74,9 +95,6 @@ export const Contacts = () => {
                            fieldData ={fieldData}
                            />
                      })()}
-                  </Grid>
-                  <Grid item xs={12}>
-                     <Paper> Pagination </Paper>
                   </Grid>
                   { isRowSelected ? <Grid item xs={12}>
                      <ContactInfo contactInfo={contactInfo}/> 
